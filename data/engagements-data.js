c51b97e6a9ce1384b82f328ebaeb7f52273d534f
nishantpatnaik10-maker <nishantpatnaik10@gmail.com>
2026-06-24 20:26:48 +1000

    Update engagements [24/06/2026, 20:26:48]

--- data/engagements-data.js
+++ data/engagements-data.js
@@ -1,5 +1,5 @@
 // ENGAGEMENTS DATA FILE
-// Auto-updated by Sync to GitHub on 2026-06-24T10:24:38.743Z
+// Auto-updated by Sync to GitHub on 2026-06-24T10:26:48.082Z
 // Do not edit manually — use the dashboard.
 
 const INITIAL_ENGAGEMENTS = [
@@ -12,5 +12,15 @@ const INITIAL_ENGAGEMENTS = [
     "attendees": "Nandana",
     "notes": "Katharina, Nandana",
     "id": "eng-1782296636255"
+  },
+  {
+    "title": "HMRC",
+    "date": "2026-06-23",
+    "endDate": "2026-06-24",
+    "gameboard": "Gameboard 3",
+    "type": "Workshop",
+    "attendees": "Rob Evans",
+    "notes": "Rob Evans",
+    "id": "eng-1782296802922"
   }
 ];
